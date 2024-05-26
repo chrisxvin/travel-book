e65e19d7dd27fb5e9a0b7af4e8255557e10e5886
Chris Liu <chris@ixvin.com>
2024-05-26 14:04:14 +0800

    添加图标显示

---
+++ src/lib/components/schedule/transport-icon.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { TransportType } from "$lib/types";
+
+export let type: TransportType = TransportType.Other;
+const map = {
+    [TransportType.Driving]: "car",
+    [TransportType.Taxi]: "taxi",
+    [TransportType.Bus]: "bus",
+    [TransportType.Metro]: "subway",
+    [TransportType.Train]: "train",
+    [TransportType.Plane]: "airplane",
+    [TransportType.Ship]: "ferry",
+    [TransportType.Walk]: "walk",
+    [TransportType.Other]: "train-car",
+};
+</script>
+
+<span class={`mdi mdi-${map[type] ?? map[TransportType.Other]}`}></span>
--- src/lib/components/schedule/transport.svelte
+++ src/lib/components/schedule/transport.svelte
@@ -2,15 +2,16 @@
 import type { ITransport } from "$lib/types";
 
 import { showTime } from "$lib/utils";
+import TransportIcon from "./transport-icon.svelte";
 
 export let tp: ITransport;
 </script>
 
 <div class="card mb-1">
-    <div class="card-header d-flex flex-row flex-wrap justify-content-between">
-        <span class="">{tp.leaveFrom}</span>
-        <span class="">--&gt;</span>
-        <span class="">{tp.arriveTo}</span>
+    <div class="card-header d-flex flex-row flex-wrap justify-content-between places">
+        <span class="from">{tp.leaveFrom}</span>
+        <span class="arror">--&gt;</span>
+        <span class="to">{tp.arriveTo}</span>
     </div>
     <ul class="list-group list-group-flush">
         <li class="list-group-item clearfix">
@@ -18,7 +19,7 @@ export let tp: ITransport;
             <span class="float-end">{tp.arriveAt != null && showTime(tp.arriveAt)}</span>
         </li>
         <li class="list-group-item clearfix">
-            <span class="float-start">{tp.serviceId}</span>
+            <span class="float-start"><TransportIcon type={tp.travelBy} /> {tp.serviceId}</span>
             <span class="float-end"><span class="badge text-bg-primary">{tp.currency}</span>&nbsp;{tp.price}</span>
         </li>
         <li class="list-group-item clearfix">
@@ -27,3 +28,15 @@ export let tp: ITransport;
         </li>
     </ul>
 </div>
+
+<style lang="less" scoped>
+.places {
+    .from {
+        margin-right: auto;
+    }
+
+    .to {
+        margin-left: auto;
+    }
+}
+</style>
