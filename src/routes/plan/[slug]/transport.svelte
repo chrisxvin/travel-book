a5d47f22c98275c8c4518da8d1d97a6bc0c23397
Chris Liu <chris@ixvin.com>
2024-06-08 14:22:19 +0800

    优化样式

--- src/routes/plan/[slug]/transport.svelte
+++ src/routes/plan/[slug]/transport.svelte
@@ -17,7 +17,7 @@ const format = new Intl.NumberFormat("zh-CN", {
     <div class="row">
         <div class="col-1">{tp.leaveAt ?? ""}</div>
         <div class="col-1 waypoint-connector">
-            <span class="mdi mdi-circle-outline"></span>
+            <span class="start mdi mdi-circle-outline"></span>
         </div>
         <div class="col">{tp.leaveFrom}</div>
     </div>
@@ -43,26 +43,6 @@ const format = new Intl.NumberFormat("zh-CN", {
     </div>
 </div>
 
-<!--
-<div class="card mb-1">
-    <div class="card-header d-flex flex-row flex-wrap justify-content-between places">
-        <span class="from">{tp.leaveFrom}</span>
-        <span class="arror">--&gt;</span>
-        <span class="to">{tp.arriveTo}</span>
-    </div>
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item clearfix">
-            <span class="float-start">{tp.leaveAt != null && showTime(tp.leaveAt)}</span>
-            <span class="float-end">{tp.arriveAt != null && showTime(tp.arriveAt)}</span>
-        </li>
-        <li class="list-group-item clearfix">
-            <span class="float-start"><TransportIcon type={tp.travelBy} /> {tp.serviceId}</span>
-            <span class="float-end">{format.format(tp.price ?? 0)}</span>
-        </li>
-    </ul>
-</div>
--->
-
 <style lang="less" scoped>
 .places {
     .from {
@@ -75,26 +55,30 @@ const format = new Intl.NumberFormat("zh-CN", {
 }
 
 .trans-icon {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
     font-size: 1.6em;
 }
 
 .waypoint-connector {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
     .start {
         height: 100%;
-        border-right: 5px solid deepskyblue;
-        margin-right: 10px;
     }
 
     .middle {
         height: 100%;
-        border-right: 5px solid deepskyblue;
-        margin-right: 10px;
+        background-color: deepskyblue;
     }
 
     .end {
         height: 100%;
-        border-right: 5px solid deepskyblue;
-        margin-right: 10px;
     }
 }
 
