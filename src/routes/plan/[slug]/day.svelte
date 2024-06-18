4407145db7d7e96834392a0c2c1b9e802939a4b1
Chris Liu <chris@ixvin.com>
2024-06-18 12:39:12 +0800

    完成编辑模式的对话框。

--- src/routes/plan/[slug]/day.svelte
+++ src/routes/plan/[slug]/day.svelte
@@ -10,21 +10,15 @@ import AddNewItem from "./add-new-item.svelte";
 import EditPlace from "./edit-place.svelte";
 import EditTransport from "./edit-transport.svelte";
 import EditActivity from "./edit-activity.svelte";
-import EditToggleButton from "./edit-toggle-button.svelte";
 import { EditMode } from "./types";
 
 export let timeline: TimelineItem[];
 
-let isEditing = false;
 let editMode: EditMode = EditMode.None;
 let editingItem: TimelineItem | undefined;
 let editingIndex = -1;
 let editingType: TimelineEntryKind = TimelineEntryKind.Unknown;
 let dlgEdit: HTMLDialogElement;
-
-function extract<T extends TimelineItem>(item: TimelineItem): T {
-    return item as T;
-}
 
 const editComps: Record<TimelineEntryKind, ConstructorOfATypedSvelteComponent> = {
     [TimelineEntryKind.Unknown]: EditPlace,
@@ -33,16 +27,16 @@ const editComps: Record<TimelineEntryKind, ConstructorOfATypedSvelteComponent> =
     [TimelineEntryKind.Activity]: EditActivity,
 };
 
-// function editItem(item: TimelineItem) {
-//     item.isEditing = true;
-//     timeline = timeline;
-//     console.log("Edit", item);
-// }
+function EditItem(index: number, item: TimelineItem) {
+    editingType = item.kind;
+    editingIndex = index;
+    editingItem = {...item};
+    editMode = EditMode.Edit;
+    dlgEdit.showModal();
+}
 
-function btnEdit_Click() {}
-
-function doAddNewItem(e: CustomEvent<AddNewItemEventArgs>) {
-    log(e);
+function btnAddNewItem_Click(e: CustomEvent<AddNewItemEventArgs>) {
+    // log(e);
     editMode = EditMode.Add;
     editingIndex = e.detail.index;
     editingType = e.detail.kind;
@@ -58,8 +52,6 @@ function doAddNewItem(e: CustomEvent<AddNewItemEventArgs>) {
         ),
     } as TimelineItem;
 
-    // timeline.splice(e.detail.index + 1, 0, newItem);
-    // timeline = timeline;
     dlgEdit.showModal();
 }
 
@@ -68,11 +60,13 @@ function btnEditSave_Click() {
 
     if (editMode === EditMode.Add) {
         timeline.splice(editingIndex + 1, 0, editingItem);
-        timeline = timeline;
-    } else if (editMode === EditMode.Edit) {
-        //
+    } else if (editMode === EditMode.Edit && editingIndex != -1) {
+        timeline[editingIndex] = editingItem;
+        editingItem = undefined;
     }
 
+    timeline = timeline;
+    editMode = EditMode.None;
     dlgEdit.close();
 }
 
@@ -96,25 +90,17 @@ function btnEditCancel_Click() {
                 <ShowActivity {item} />
             {/if}
 
-            <EditToggleButton on:prepare={btnEdit_Click} bind:isEditing />
-
-            <!-- TODO: 这个添加按钮，能否做成单独的，根据 hover 的 item 来显示 -->
-            <AddNewItem index={i} on:add={doAddNewItem} />
-            <!--
-            <button class="btn btn-circle text-3xl add-btn-fix">
-                <span class="mdi mdi-plus"></span>
+            <!-- TODO: 这个编辑和下面的添加按钮，能否做到列表之外，根据 hover 的 item 来显示 -->
+            <button class="btn btn-xs edit-btn-fix timeline-end" on:click={() => EditItem(i, item)}>
+                <span class="mdi mdi-pencil"></span>
             </button>
-            -->
+
+            <AddNewItem index={i} on:add={btnAddNewItem_Click} />
 
             {#if i !== timeline.length}<hr />{/if}
         </li>
     {/each}
 </ul>
-<!-- 
-<EditPlace item={extract<IPlaceViewModel>(editingItem)} />
-<EditTransport item={extract<ITransportViewModel>(editingItem)} />
-<EditActivity item={extract<IActivityViewModel>(editingItem)} />
--->
 
 <!-- Open the modal using ID.showModal() method. can be closed using ID.close() method -->
 <!-- <button class="btn" onclick="dlgEdit.showModal()">open modal</button> -->
